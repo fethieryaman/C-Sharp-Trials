94f72878a09aebfbfabfa5e8e4a227c91937eef0
Fethi Eryaman <fethi.eryaman@bil.omu.edu.tr>
2017-11-27 11:53:55 +0300

    Add files via upload

---
+++ bilgisayar ağları/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ip_information = new System.Windows.Forms.Label();
+            this.ipadress = new System.Windows.Forms.Label();
+            this.webView1 = new EO.WebBrowser.WebView();
+            this.webControl1 = new EO.WinForm.WebControl();
+            this.webView2 = new EO.WebBrowser.WebView();
+            this.check_internet = new System.Windows.Forms.Label();
+            this.ip_information_title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ip_information
+            // 
+            this.ip_information.AutoSize = true;
+            this.ip_information.Location = new System.Drawing.Point(39, 446);
+            this.ip_information.Name = "ip_information";
+            this.ip_information.Size = new System.Drawing.Size(70, 13);
+            this.ip_information.TabIndex = 0;
+            this.ip_information.Text = "ip Information";
+            // 
+            // ipadress
+            // 
+            this.ipadress.AutoSize = true;
+            this.ipadress.Location = new System.Drawing.Point(549, 364);
+            this.ipadress.Name = "ipadress";
+            this.ipadress.Size = new System.Drawing.Size(57, 13);
+            this.ipadress.TabIndex = 1;
+            this.ipadress.Text = "MacAdresi";
+            // 
+            // webView1
+            // 
+            this.webView1.ObjectForScripting = null;
+            // 
+            // webControl1
+            // 
+            this.webControl1.BackColor = System.Drawing.Color.White;
+            this.webControl1.Location = new System.Drawing.Point(42, 12);
+            this.webControl1.Name = "webControl1";
+            this.webControl1.Size = new System.Drawing.Size(700, 325);
+            this.webControl1.TabIndex = 3;
+            this.webControl1.Text = "webControl1";
+            this.webControl1.WebView = this.webView2;
+            // 
+            // webView2
+            // 
+            this.webView2.ObjectForScripting = null;
+            // 
+            // check_internet
+            // 
+            this.check_internet.AutoSize = true;
+            this.check_internet.Location = new System.Drawing.Point(39, 364);
+            this.check_internet.Name = "check_internet";
+            this.check_internet.Size = new System.Drawing.Size(78, 13);
+            this.check_internet.TabIndex = 4;
+            this.check_internet.Text = "check_internet";
+            // 
+            // ip_information_title
+            // 
+            this.ip_information_title.AutoSize = true;
+            this.ip_information_title.Location = new System.Drawing.Point(39, 403);
+            this.ip_information_title.Name = "ip_information_title";
+            this.ip_information_title.Size = new System.Drawing.Size(93, 13);
+            this.ip_information_title.TabIndex = 5;
+            this.ip_information_title.Text = "ip Information Title";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 603);
+            this.Controls.Add(this.ip_information_title);
+            this.Controls.Add(this.check_internet);
+            this.Controls.Add(this.webControl1);
+            this.Controls.Add(this.ipadress);
+            this.Controls.Add(this.ip_information);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ip_information;
+        private System.Windows.Forms.Label ipadress;
+        private EO.WebBrowser.WebView webView1;
+        private EO.WinForm.WebControl webControl1;
+        private EO.WebBrowser.WebView webView2;
+        private System.Windows.Forms.Label check_internet;
+        private System.Windows.Forms.Label ip_information_title;
+    }
+}
+
---
+++ bilgisayar ağları/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int ipInformationNumber = 4;
+        String[] submasks, ips, connectionName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            ipadress.Text = "MAC adresi : " + getMacAdress();
+            ip_information.Text = "";
+            ip_information_title.Text = "ip information : ";
+            getIpAddress();
+            for (int i = 0; i<ipInformationNumber; i++)
+                ip_information.Text += connectionName[i] +  "   IP adress :   " + ips[i] + " SUBNET MASK:   " + submasks[i] + "\n";
+            
+            if (checkInternetConnection())
+            {
+                check_internet.Text = "Connect To Internet ...";
+                String[] coords = getCoords();
+                webControl1.WebView.Url = "http://www.google.com/maps?q=" + coords[0] + ",+" + coords[1]; ;
+            }
+            else
+            {
+                check_internet.Text = "Connect Not Internet ...";
+                webControl1.Hide();
+            }
+ 
+        }
+
+
+        public string getMacAdress()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            String macAdress = string.Empty;
+            foreach (NetworkInterface adapter in nics)
+            {
+                if (macAdress == String.Empty)
+                {                 
+                    macAdress = adapter.GetPhysicalAddress().ToString();
+                }
+            }
+            return macAdress;
+        }
+
+        static public string[] getCoords()
+        {
+            String[] coords = new String[2];
+            JObject jObject = JObject.Parse(new WebClient().DownloadString("http://ip-api.com/json"));
+            var coordLat = (long)jObject["lat"];
+            var coordLon = (long)jObject["lon"];
+            coords[0] = coordLat.ToString();
+            coords[1] = coordLon.ToString();
+            return coords;
+        }
+
+        public void getIpAddress()
+        {
+            submasks = new String[4];
+            ips = new String[4];
+            connectionName = new String[4];
+            int i = 0;
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                {                
+                    foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            connectionName[i] = ni.Name;
+                            ips[i] = ip.Address.ToString();
+                            submasks[i] = ip.IPv4Mask.ToString();
+                            i += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+
+   
+        public static bool checkInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (var stream = client.OpenRead("http://www.google.com"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+
+    
+}
+
+
